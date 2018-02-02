0c421a70a505c029a31f9c38976e8c6085fd4f61
Rob-MFn-Fletcher <robroy.fletcher@gmail.com>
2018-02-02 17:12:31 -0500

    Added Flask-Blogger

--- app.py
+++ app.py
@@ -1,11 +1,74 @@
 import flask
+from sqlalchemy import create_engine, MetaData
+from flask_login import UserMixin, LoginManager, login_user, logout_user
+from flask_blogging import SQLAStorage, BloggingEngine
 
-application = flask.Flask(__name__)
+app = flask.Flask(__name__)
+app.config["SECRET_KEY"] = "secret"  # for WTF-forms and login
+app.config["BLOGGING_URL_PREFIX"] = "/blog"
+#app.config["BLOGGING_DISQUS_SITENAME"] = "test"
+app.config["BLOGGING_SITEURL"] = "https://0.0.0.0:8080"
+app.config["BLOGGING_SITENAME"] = "Robs Blog"
+app.config["BLOGGING_KEYWORDS"] = ["blog", "meta", "keywords"]
+app.config["FILEUPLOAD_IMG_FOLDER"] = "fileupload"
+app.config["FILEUPLOAD_PREFIX"] = "/fileupload"
+app.config["FILEUPLOAD_ALLOWED_EXTENSIONS"] = ["png", "jpg", "jpeg", "gif"]
 
-@application.route('/')
+# extensions
+engine = create_engine('sqlite:///tmp/blog.db')
+meta = MetaData()
+sql_storage = SQLAStorage(engine, metadata=meta)
+blog_engine = BloggingEngine(app, sql_storage)
+login_manager = LoginManager(app)
+meta.create_all(bind=engine)
+
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
+
+    def get_name(self):
+        return "Rob Roy Fletcher"  # typically the user's name
+
+@login_manager.user_loader
+@blog_engine.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+index_template = """
+<!DOCTYPE html>
+<html>
+    <head> </head>
+    <body>
+        {% if current_user.is_authenticated %}
+            <a href="/logout/"> Logout </a>
+        {% else %}
+            <a href="/login/"> Login </a>
+        {% endif %}
+        &nbsp&nbsp<a href="/blog/"> Blog </a>
+        &nbsp&nbsp<a href="/blog/sitemap.xml">Sitemap</a>
+        &nbsp&nbsp<a href="/blog/feeds/all.atom.xml">ATOM</a>
+        &nbsp&nbsp<a href="/fileupload/">FileUpload</a>
+    </body>
+</html>
+"""
+
+
+@app.route('/')
 def homepage():
-    #return flask.render_template('index.html')
-    return "hello world"
+    #return flask.render_template('dummyContent.html')
+    return flask.render_template_string(index_template)
+    #return "hello world"
+
+@app.route("/login/")
+def login():
+    user = User("testuser")
+    login_user(user)
+    return flask.redirect("/blog")
+
+@app.route("/logout/")
+def logout():
+    logout_user()
+    return flask.redirect("/")
 
 if __name__=='__main__':
-    application.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=8080)
