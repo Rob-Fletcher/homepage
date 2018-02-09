4436a2594e1f4582dad2d008a99068b955846d87
Rob-MFn-Fletcher <robroy.fletcher@gmail.com>
2018-02-09 13:12:06 -0500

    Filling more out. Added downloadable CV

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ def homepage():
 def atlas():
     return flask.render_template('atlas.html')
 
+@app.route('/download')
+def download():
+    return flask.send_file('static/RRFletcher_CV.pdf')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=8080)
